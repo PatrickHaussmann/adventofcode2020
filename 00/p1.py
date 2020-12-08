b2efcbe0849d8fa671d318a1a95919567c6c312a
PatrickHaussmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-12-08 08:42:36 +0100

    updated template

--- 00/p1.py
+++ 00/p1.py
@@ -1,6 +1,2 @@
-lines = []
 with open('00/input.txt', 'r') as reader:
-    for line in reader:
-        line = line.strip('\n')
-        line = line.strip('\t')
-        lines.append(line)
+    lines = [line.strip() for line in reader if line.strip()]
